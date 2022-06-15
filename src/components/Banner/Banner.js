0d97dc9bb24a04dd6d63ac9f4b57d9f94b6d64c8
collins felix <cllnsflx40@gmail.com>
2022-06-15 05:32:50 -0700

    made it a little bit responsive

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,27 +1,50 @@
-import { Button } from 'bootstrap'
-import React from 'react'
-import styled from 'styled-components'
-import img1 from "../../assets/amazonguy.jpg"
+import { Button } from "react-bootstrap";
+import React from "react";
+import styled from "styled-components";
+import img1 from "../../assets/img3.png";
 
 const Banner = () => {
   return (
     <Main>
-        <div>
-            <p>Buy from us today</p>
-            <h1>Lorem Ipsum some dummy text</h1>
-            <p>some text i dont even understand</p>
-            <Button>Shop Now</Button>
-        </div>
-        <div>
-            <img src={img1} />
-        </div>
+      <div className="left">
+        <span>Buy from us today</span>
+        <span>Lorem Ipsum some dummy text</span>
+        <p>some text i dont even understand</p>
+        <Button id="btn">Shop Now</Button>
+      </div>
+      <div>
+        <img src={img1} />
+      </div>
     </Main>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
 
 const Main = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #d9c45285;
+  width: 100%;
+  height: auto;
+  padding-top: 20px ;
+  margin-top: 20px;
+  overflow: hidden;
+  .left {
+    display: flex;
+    flex-direction: column;
+  }
+  .left span:nth-child(2) {
+    font-size: 40px;
+    width: 88%;
+  }
+  img {
+    width: 400px;
+    height: 400px;
+  }
 
-
-`
+  #btn {
+    width: 25%;
+  }
+`;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -78,5 +78,12 @@ const Main = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-gap: 20px;
+
+    @media (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+    }
+    @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+    }
   }
 `;
